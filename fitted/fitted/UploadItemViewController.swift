cd3554ffef2fb95cfb503d1cb34c032474d42462
sarahfedorchak <sarahfedorchak@utexas.edu>
2025-11-11 12:04:23 -0600

    upload item VC storyboard

---
+++ fitted/fitted/UploadItemViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UploadItemViewController.swift
+//  fitted
+//
+//  Created by Sarah Neville on 11/11/25.
+//
+
+import UIKit
+
+class UploadItemViewController: UIViewController {
+
+    @IBOutlet weak var addItemButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var uploadImageButton: UIButton!
+    @IBOutlet weak var descriptionText: UITextView!
+    @IBOutlet weak var sizeField: UITextField!
+    @IBOutlet weak var rentalPriceField: UITextField!
+    @IBOutlet weak var brandField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
