78aeb5688680b5aac25be54acd8b6399d15eae06
sarahfedorchak <sarahfedorchak@utexas.edu>
2025-11-11 19:46:06 -0600

    group settings storyboard

---
+++ fitted/fitted/GroupSettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GroupSettingsViewController.swift
+//  fitted
+//
+//  Created by Sarah Neville on 11/11/25.
+//
+
+import UIKit
+
+class GroupSettingsViewController: UIViewController {
+
+    @IBOutlet weak var groupPassLabel: UILabel!
+    @IBOutlet weak var groupIdLabel: UILabel!
+    @IBOutlet weak var groupNameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func manageButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func editImagePressed(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
